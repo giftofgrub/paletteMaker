b8fad2302bdfeb84d74481207d26cb726b1735a2
Gabriel <gab.atienza@gmail.com>
2019-10-16 14:51:22 -0700

    create-react-app, seedPalette

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* navbar goes here */}
+        <div className="Palette-colors">
+        
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Palette;
