4d12a4252370b74108abdb7368ecbb9530e1aa21
Gabriel <gab.atienza@gmail.com>
2019-12-04 09:47:25 -0800

    tweak classes, styles of paletteForm

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -8,9 +8,9 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import styles from './styles/PaletteFormNavStyles';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 class PaletteFormNav extends Component {
   constructor(props) {
@@ -57,8 +57,9 @@ class PaletteFormNav extends Component {
               onClick={handleDrawerOpen}
               edge="start"
               className={clsx(classes.menuButton, open && classes.hide)}
+              // alternative: className={clsx(classes.menuButton, {[classes.hide] : open})}
             >
-              <MenuIcon />
+              <ChevronRightIcon />
             </IconButton>
             <Typography variant="h6" noWrap>
               Create A Palette
--- src/styles/NewPaletteFormStyles.js
+++ src/styles/NewPaletteFormStyles.js
@@ -22,6 +22,7 @@ const useStyles = makeStyles(theme => ({
   drawerHeader: {
     display: 'flex',
     alignItems: 'center',
+    width: "100%",
     padding: theme.spacing(0, 1),
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
@@ -29,7 +30,8 @@ const useStyles = makeStyles(theme => ({
   content: {
     flexGrow: 1,
     height: "calc(58vh - 64px)",
-    padding: theme.spacing(3),
+    // (default) padding: theme.spacing(3),
+    padding: 0,
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
