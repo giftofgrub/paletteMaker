7337958ba475733b98702a34317efcab7c72fb75
Gabriel <gab.atienza@gmail.com>
2019-12-04 11:21:14 -0800

    add responsive helper and styled colorBox

---
+++ src/styles/sizes.js
@@ -0,0 +1,19 @@
+// @media (max-width: 575.98px) { ... }
+// @media (max-width: 767.98px) { ... }
+// @media (max-width: 991.98px) { ... }
+// @media (max-width: 1199.98px) { ... }
+
+
+export default {
+  
+  
+  down(size) {
+    const sizes = {
+      xs: "575.98px",
+      sm: "767.98px",
+      md: "991.98px",
+      lg: "1199.98px"
+    }
+    return `@media (max-width: ${sizes[size]})`
+  }
+}
